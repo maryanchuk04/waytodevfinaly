496c338a39c314c7c6b657aaa4792a7f5414cc65
Serhiy Dvoryannikov <dvoryannikovserhiy@gmail.com>
2021-11-07 18:45:08 +0200

    posts styling, courses rendering

--- WayToDev/Frontend/WayToDev/client/src/components/AboutLang.js
+++ WayToDev/Frontend/WayToDev/client/src/components/AboutLang.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import './AboutLang.css'; 
+import React from "react";
+import "./AboutLang.css";
 
 function AboutLang() {
-    return (
-        <div class = "languages">
-            <div class = "container">
-                  <div class = "pictures">
-                    <img src = "https://i.ibb.co/3RVXdW7/0-02-05-b29e4502427b2086c7cb33c360e579acdb0b47569d8cfaf6bae2287a57aadb20-7ee29e28cab4ff8b.png" alt="programming" height = "350px" ></img> 
-                  </div>
-          
-                <div class = "text">
-                    <div class = "text_title"><h1>Still haven't decided on the right programming language for you?</h1></div>
-                    <div class = "text_subtitle"><p>If you still haven't decided which programming language you like best, or you just don't know what to choose, we can help you ...</p></div>
-                    <a href="#" class="banerButton_1 banerReadMore_1">
+	return (
+		<div class="languages">
+			<div class="container">
+				<div class="pictures">
+					<img
+						src="https://i.ibb.co/3RVXdW7/0-02-05-b29e4502427b2086c7cb33c360e579acdb0b47569d8cfaf6bae2287a57aadb20-7ee29e28cab4ff8b.png"
+						alt="programming"
+						height="350px"
+					></img>
+				</div>
+
+				<div class="text">
+					<div class="text_title">
+						<h1>
+							Still haven't decided on the right programming
+							language for you?
+						</h1>
+					</div>
+					<div class="text_subtitle">
+						<p>
+							If you still haven't decided which programming
+							language you like best, or you just don't know what
+							to choose, we can help you ...
+						</p>
+					</div>
+					<a href="#" class="langBtn">
 						Read More
 					</a>
-                </div>
-            </div>
-        </div>
-    )
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default AboutLang
+export default AboutLang;
