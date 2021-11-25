263a97c24cffa08ddf59c2ad6c6da605e823f6c4
Maks <lion20914king@gmail.com>
2021-11-25 14:43:58 +0200

    Auth form add

--- WayToDev/Frontend/WayToDev/client/src/components/Login/components/LoginWindow.js
+++ WayToDev/Frontend/WayToDev/client/src/components/Login/components/LoginWindow.js
@@ -2,50 +2,59 @@ import React, { useState } from "react";
 import "./LoginWindow.css"
 
 function LoginWindow() {
-    const [formData, setFormData] = useState({    
-        email: "",
-        password: ""
-      });
+    const [signInUp, setSignInUp] = useState(false);
     
-      const updateFormData = event =>
-        setFormData({
-          ...formData,
-          [event.target.name]: event.target.value
-        });
-    
-      const {  email, password } = formData;
     return (
         <div>
-            <div className="allcomponents">
-                <div className="singinblock">
-                    
-                        <h1>SIGN <span>IN</span></h1>                              
-                    <div className="formablock">
-                    <form>                                      
-                        <input
-                            value={email}
-                            onChange={e => updateFormData(e)}
-                            placeholder="Username or Email"
-                            type="email"
-                            name="email"
-                            required
-                        />
-                        <input
-                            value={password}
-                            onChange={e => updateFormData(e)}
-                            placeholder="Password"
-                            type="password"
-                            name="password"
-                            required
-                        />
-                            <button type="submit">Sign IN</button>
-                        </form>
-                    </div>
-                </div>
-                <div className="picture_block">
-                    <img src="https://i.ibb.co/kmm6xxD/new.jpg" alt="new" border="0"></img>
-                </div>
-            </div>
+        <div className="allcomponents">
+        <div class={`container ${signInUp && 'right-panel-active'}`} id="container">
+	<div class="form-container sign-up-container">
+		<form action="#">
+			<h1>Create Account</h1>
+			<div class="social-container">
+				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+				<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+			</div>
+			<span>or use your email for registration</span>
+			<input type="text" placeholder="Name" />
+			<input type="email" placeholder="Email" />
+			<input type="password" placeholder="Password" />
+			<button>Sign UP</button>
+		</form>
+	</div>
+	<div class="form-container sign-in-container">
+		<form action="#">
+			<h1>Sign IN</h1>
+			<div class="social-container">
+				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+				<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+			</div>
+			<span>or use your account</span>
+			<input type="email" placeholder="Email" />
+			<input type="password" placeholder="Password" />
+			<a href="#">Forgot your password?</a>
+			<button>Sign In</button>
+		</form>
+	</div>
+	<div class="overlay-container">
+		<div class="overlay">
+			<div class="overlay-panel overlay-left">
+				<h1>Welcome Back!</h1>
+				<p>To keep connected with us please login with your personal info</p>
+				<button class="ghost" id="signIn" onClick={() => setSignInUp(!signInUp)}>Sign In</button>
+			</div>
+			<div class="overlay-panel overlay-right">
+				<h1>Hello, Friend!</h1>
+				<p>Enter your personal details and start journey with us</p>
+				<button class="ghost" id="signUp" onClick={() => setSignInUp(!signInUp)}>Sign Up</button>
+			</div>
+		</div>
+	</div>
+</div>
+
+        </div>
         </div>
     )
 }
