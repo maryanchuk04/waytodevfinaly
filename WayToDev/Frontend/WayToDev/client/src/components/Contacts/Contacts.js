a9a4b96960429c2ed66e6b38537a008e3092ba3e
Serhiy Dvoryannikov <dvoryannikovserhiy@gmail.com>
2021-11-27 19:27:06 +0200

    redo styling of slider, and changing font-weight for header

--- WayToDev/Frontend/WayToDev/client/src/components/Contacts/Contacts.js
+++ WayToDev/Frontend/WayToDev/client/src/components/Contacts/Contacts.js
@@ -18,63 +18,65 @@ function Contacts() {
 	return (
 		<div className="contacts">
 			<div className="container">
-				<Slider {...sliderSettings}>
-					{devs.map((developer) => (
-						<div className="contactsCard">
-							<img
-								src={developer.img}
-								alt={developer.name}
-								className="contactsImg"
-							/>
-							<div className="contactsInfo">
-								<h6>Hello everyone, I am</h6>
-								<h1>{developer.name}</h1>
-								<h6>{developer.position}</h6>
-								<p>{developer.description}</p>
+				<div className="contactsCard">
+					<Slider {...sliderSettings}>
+						{devs.map((developer) => (
+							<div className="contactsFlex">
+								<img
+									src={developer.img}
+									alt={developer.name}
+									className="contactsImg"
+								/>
+								<div className="contactsInfo">
+									<h6>Hello everyone, I am</h6>
+									<h1>{developer.name}</h1>
+									<h6>{developer.position}</h6>
+									<p>{developer.description}</p>
 
-								<div className="contactsPrivateInfo">
-									<div>
-										<i class="far fa-calendar-alt"></i>
-										{developer.birthday}
+									<div className="contactsPrivateInfo">
+										<div>
+											<i class="far fa-calendar-alt"></i>
+											{developer.birthday}
+										</div>
+										<div>
+											<i class="fas fa-phone"></i>
+											{developer.phone}
+										</div>
+										<div>
+											<i class="far fa-envelope"></i>
+											{developer.email}
+										</div>
+										<div>
+											<i class="far fa-address-card"></i>
+											{developer.address}
+										</div>
 									</div>
-									<div>
-										<i class="fas fa-phone"></i>
-										{developer.phone}
-									</div>
-									<div>
-										<i class="far fa-envelope"></i>
-										{developer.email}
-									</div>
-									<div>
-										<i class="far fa-address-card"></i>
-										{developer.address}
-									</div>
-								</div>
 
-								<div className="contactsSocials">
-									<a
-										href={developer.socials.facebook}
-										target="_blank"
-										className="facebook">
-										<i class="fab fa-facebook-f"></i>
-									</a>
-									<a
-										href={developer.socials.instagram}
-										target="_blank"
-										className="instagram">
-										<i class="fab fa-instagram"></i>
-									</a>
-									<a
-										href={developer.socials.github}
-										target="_blank"
-										className="github">
-										<i class="fab fa-github"></i>
-									</a>
+									<div className="contactsSocials">
+										<a
+											href={developer.socials.facebook}
+											target="_blank"
+											className="facebook">
+											<i class="fab fa-facebook-f"></i>
+										</a>
+										<a
+											href={developer.socials.instagram}
+											target="_blank"
+											className="instagram">
+											<i class="fab fa-instagram"></i>
+										</a>
+										<a
+											href={developer.socials.github}
+											target="_blank"
+											className="github">
+											<i class="fab fa-github"></i>
+										</a>
+									</div>
 								</div>
 							</div>
-						</div>
-					))}
-				</Slider>
+						))}
+					</Slider>
+				</div>
 			</div>
 		</div>
 	);
